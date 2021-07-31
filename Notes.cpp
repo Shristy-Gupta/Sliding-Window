6de0ba3de2cd2485d4883ea9e8473afb55ba8358
Shristy Gupta <shristysgupta@gmail.com>
2021-07-31 21:38:12 +0530

    Update Notes.cpp

--- Notes.cpp
+++ Notes.cpp
@@ -17,6 +17,39 @@ int maximumSumSubarray(int K, vector<int> &Arr , int N){
         return max_sum;
     }
 3) First negative in every min size of k
+vector<long long> printFirstNegativeInteger(long long int A[],
+                                             long long int N, long long int K) {
+            vector<long long> ans;
+            queue<long long> q;
+            long long int i;
+            for(i=0;i<K;i++){
+                if(A[i]<0){
+                    //Adding only the index of the negative element
+                    q.push(i);
+                }
+            }
+            for(;i<N;i++){
+                if(!q.empty()){
+                    ans.push_back(A[q.front()]);
+                }
+                else{
+                    ans.push_back(0);
+                }
+                while(!q.empty() && q.front()<i-K+1){
+                    q.pop();
+                }
+                if(A[i]<0){
+                    q.push(i);
+                }
+            }
+            if(!q.empty()){
+                ans.push_back(A[q.front()]);
+            }
+            else{
+                ans.push_back(0);
+            }
+            return ans;                                     
+ }
 4) Count occurance of Anagram
 5) Maximum of all sub-array of size k
 6) Max and min of every window size
