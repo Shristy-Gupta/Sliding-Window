dd6e898a5b7e4570bbf7b9d6ca894c80a6977fa3
Shristy Gupta <shristysgupta@gmail.com>
2021-08-03 21:07:05 +0530

    Update Notes.cpp

--- Notes.cpp
+++ Notes.cpp
@@ -50,7 +50,52 @@ vector<long long> printFirstNegativeInteger(long long int A[],
             }
             return ans;                                     
  }
-4) Count occurance of Anagram
+4) Count occurance of Anagram: https://www.youtube.com/watch?v=MW4lJ8Y0xXk&list=PL_z_8CaSLPWeM8BDJmIYDaoQ5zuwyxnfj&index=5
+
+// Generate a map of all character occurances of the 2nd string, i.e char and its count.
+// The window size becomes the length of 2nd string, as the anangrams wil also be of this length, amd another variable count denotes the distinct characters left in a sliding window.
+// In a window if the current character is present in map, then decrease its count, and if the count becomes 0, decrease the 'count' variable.
+// If the window size is lesser than k just move forward the window
+// When the window is hit ->
+// If the count is 0, it means all the characters needed for its anagram is here in it, therefore increase your answer
+// To remove the last part (arr[i] part) so that the window can be shifted ->
+// If the s[i] is present in map then increase its count and if it changes from 0->1 then increase the 'count' variable
+
+int solve(string s, string ana){
+
+    unordered_map<char, int> m;
+    for(auto it : ana) m[it]++;
+
+    int window_Size=ana.length();
+    int count=m.size();
+    
+    int start=0, end=0;
+    int ans=0;
+    
+    while(end<s.length()){
+    
+        if(m.find(s[end])!=m.end()){
+            m[s[end]]--;
+            if(m[s[end]]==0) count--;
+        }
+        //only for growing phase
+        if((end-start+1)<window_Size) end++;
+        else if((end-start+1)==window_Size){
+            if(count==0) ans++;
+            if(m.find(s[start])!=m.end()){
+                m[s[start]]++;
+                if(m[s[start]]==1) count++;
+            }
+            start++;
+            end++;
+        }
+    }
+    return ans;
+}
+
+// TC : O(n)
+// SC : O(distinctChars(ana))
+
 5) Maximum of all sub-array of size k
 6) Max and min of every window size
 
